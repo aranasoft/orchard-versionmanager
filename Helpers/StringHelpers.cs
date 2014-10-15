a787c52a155b5c020a5adc072c83455ae0eaed5c
Jay Harris <jay@aranasoft.com>
2014-10-15 09:06:19 -0400

    HTML Cleanup. Usability Enhancements to deleted list.

---
+++ Helpers/StringHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arana.VersionManager.Helpers {
+    public class StringHelpers {
+        public static string NonNullOrEmpty(params string[] values) {
+            return values.FirstOrDefault(value => !string.IsNullOrEmpty(value));
+        }
+    }
+}
